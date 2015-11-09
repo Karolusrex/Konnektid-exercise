41b86c20a3b667d6460b2b08e5634a695c3f7ce0
Karolusrex <kalle.lundfall@gmail.com>
2015-11-09 12:21:00 +0100

    Home added

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default React.createClass({
+  render() {
+    return (
+      <div>
+        <h2>Home</h2>
+        <p>This web app was made as an excercise for a job application on behalf of <a href = "https://www.konnektid.com/">Konnektid</a> by Karl Lundfall.</p>
+      </div>
+    );
+  }
+});
--- src/routes.js
+++ src/routes.js
@@ -4,10 +4,12 @@ import { Route } from 'react-router'
 import App from './components/App'
 import About from './components/About'
 import PoweredBy from './components/Powered-by'
+import Home from './components/Home'
 
 export default (
     <Route path="/" component={App}>
       <Route path="about" component={About}/>
+        <Route path="home" component={Home}/>
       <Route path="poweredby" component={PoweredBy}/>
     </Route>
 );
