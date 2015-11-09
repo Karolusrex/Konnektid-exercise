05730676becbd266637ced49be38e4b3cfe727f9
Karolusrex <kalle.lundfall@gmail.com>
2015-11-09 12:20:26 +0100

    Bootstrap menu

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,47 @@
 import React from 'react';
-import { Link }  from 'react-router';
-import packageJSON from '../../package.json';
-
+import Navbar from 'react-bootstrap/lib/Navbar';
+import NavBrand from 'react-bootstrap/lib/NavBrand';
+import Nav from 'react-bootstrap/lib/Nav';
+import Grid from 'react-bootstrap/lib/Grid';
+import NavItem from 'react-bootstrap/lib/NavItem';
+import {IndexLinkContainer} from 'react-router-bootstrap';
+import Home from './Home';
 
 export default React.createClass({
   returnSomething(something) {
     //this is only for testing purposes. Check /test/components/App-test.js
     return something;
   },
+    generateMenuItem(link,text,eventKey, query={}) {
+        
+        return (
+            <IndexLinkContainer to={link} >
+              <NavItem href="#" eventKey={eventKey}>{text}</NavItem>
+            </IndexLinkContainer>
+            );
+    },
   render() {
-    const version = packageJSON.version;
-
+      const childContent = this.props.children || (<Home/>);
     return (
+        
       <div>
+        
+            
         <header>
-          <h1>React Starterify {version}</h1>
-          
-            <Link to="/todo">Home</Link>
-        <Link to="/about">About</Link>
-          <Link to="/poweredby">Powered by</Link>
+        <Navbar>
+            <NavBrand><a href="#">Todo or notodo</a></NavBrand>
+            <Nav>
+                {this.generateMenuItem("/","Home",1)}
+                {this.generateMenuItem("about","About",2)}
+                {this.generateMenuItem("poweredby","Powered by",3)}
+
+            </Nav>
+          </Navbar>
         
         </header>
-        <section>
-          {this.props.children || 'Welcome to React Starterify'}
-        </section>
+        <Grid>
+                {childContent}
+        </Grid>
       </div>
     )
   }
