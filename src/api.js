960549a196b30778b8055cf250cf38087cf10878
Karolusrex <kalle.lundfall@gmail.com>
2015-11-11 00:10:45 +0100

    api supports todo list and todos

--- src/api.js
+++ src/api.js
@@ -5,7 +5,53 @@ export function addTodoList(req,res)  {
     let TodoList = mongoose.model('Todo-list');
     new TodoList(req.body).save((error,model) => {
         res.send(model);
-       
     });
+}
+
+export function addTodo(req,res)  {
+    let TodoItem = mongoose.model('Todo-item');
+    let TodoList = mongoose.model('Todo-list');
+    new TodoItem(req.body).save((error,todoItemModel) => {
+        TodoList.findByIdAndUpdate(
+            req.body.listId,
+            {$push: {"items":  todoItemModel._id}},
+            function(err, model) {
+                if(!err){
+                    res.send(todoItemModel);
+                } else {
+                    console.log(err);
+                }
+            }
+        );
+    });
+}
     
+export function deleteTodo(req,res)  {
+    let TodoItem = mongoose.model('Todo-item');
+    let TodoList = mongoose.model('Todo-list');
+    new TodoItem(req.body).remove((error,todoItemModel) => {
+        TodoList.findByIdAndUpdate(
+            req.body.listId,
+            { $pullAll: {items: [todoItemModel._id] } },
+            function(err, model) {
+                if(!err){
+                    res.send(todoItemModel);
+                } else {
+                    console.log(err);
+                }
+            }
+        );
+    });
+}
+
+    
+export function deleteTodoList(req,res)  {
+    let TodoList = mongoose.model('Todo-list');
+    new TodoList(req.body).remove((error,report) => {
+        if(!error){
+            res.send({status:"ok", report});
+        } else {
+            res.send({status:"error", error});
+        }
+    });
 }
--- test/api/api-test.js
+++ test/api/api-test.js
@@ -4,8 +4,6 @@ import { expect, assert } from 'chai';
 import About from '../../src/components/About';
 import server from '../../src/server';
 import config from '../../src/config';
-import rest from 'rest';
-import mime from 'rest/interceptor/mime';
 import errorCode  from 'rest/interceptor/errorCode';
 import pathPrefix from 'rest/interceptor/pathPrefix';
 import {Client} from 'node-rest-client';
@@ -13,28 +11,53 @@ import request from 'request-json';
 import mongoose from 'mongoose';
 import should from 'should';
 
-let client = rest.wrap(mime)
-    .wrap(pathPrefix, { prefix: 'http://localhost' });
 
-describe('API', () => {
+describe('API', function() {
     before(function(done){
         server((server) => {
             this.server = server;
-            
             this.TodoList = mongoose.model('Todo-list');
             this.TodoItem = mongoose.model('Todo-item');
             this.client = request.createClient('http://localhost:' + config.port + '/');
+            
+            this.saveTodoList = function(cb) {
+                this.client.post('/addTodoList',{text:"shopping list"},(err,res,body) => {
+                    expect(err).to.be.null;
+                    expect(body.text).to.be.equal('shopping list');
+                    expect(body._id).to.not.be.undefined;
+                    cb(err,body);
+                });
+            };
+            
             done();
         });
     });
-
     
+it('it should respond with added todo when querying /addTodo and the item should be consistent in the database with the Todolist', function(done) {
+    this.saveTodoList((errorAlwaysNull,todoListReply) => {
+        this.client.post('/addTodo',{text:"do this and that",listId:todoListReply._id},(err,res,todoReply) => {
+            expect(err).to.be.null;
+            expect(todoReply.text).to.be.equal('do this and that');
+            expect(todoReply._id).to.not.be.undefined;
+            this.TodoList.findOne({_id: todoListReply._id},(error,result) => {
+                expect(error).to.be.null;
+                
+                expect(result.items).to.include(todoReply._id);
+                todoReply.listId = todoListReply._id;
+                this.client.post("/deleteTodo",todoReply,(error,result) => {
+                    expect(error).to.be.null;
+                    this.TodoList.findOne({_id: todoListReply._id},(error,result) => {
+                        expect(result.items).not.to.include(todoReply._id);
+                        done();
+                    });
+                });
+            });
+        });
+    });
+});
+
   it('should respond with added todolist when querying /addTodoList and the item should be in the database', function(done) {
-      
-      this.client.post('/addTodoList',{text:"shopping list"},(err,res,body) => {
-         expect(err).to.be.null;
-          expect(body.text).to.be.equal('shopping list');
-          expect(body._id).to.not.be.undefined;
+      this.saveTodoList((errorAlwaysNull,body) => {
           this.TodoList.find({},(error,results) => {
               //get the latest result
               let todoList = results.slice(-1)[0];
@@ -58,12 +81,13 @@ describe('API', () => {
   });
     
     it('should be able to add a todolist, then delete it',function(done){
-        this.client.post('/addTodoList',{text:"shopping list"},(err,res,body) => {
-            expect(err).to.be.null;
-            expect(body.text).to.be.equal('shopping list');
-            expect(body._id).to.not.be.undefined;
-            this.client.post('/removeTodoList',body,(err,res,body) => {
+        this.saveTodoList((errorAlwaysNull,body) => {
+            this.client.post('/deleteTodoList',body,(err,res,body) => {
                 expect(body.status).to.equal("ok");
+                this.TodoList.find({},(error,results) => {
+                    expect(results.length).to.equal(0);
+                    done();
+                });
             });
         });
     });
